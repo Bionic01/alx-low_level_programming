64b5442f9f48aa2c6a746e70a6cce72ca85bc124
Rodney <rmjesiman@gmail.com>
2022-03-13 16:37:14 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ * File 4-puts.c
+ * Rule: use puts NOT printf to print text
+ */
+#include <stdio.h>
+/*
+ * main - Prints "Programming is like building a multilingual puzzle"
+ * followed by a new line.
+ * returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

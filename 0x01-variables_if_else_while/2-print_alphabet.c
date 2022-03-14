a253b7c1df15f90419e57004054325ea8d962721
Rodney <rmjesiman@gmail.com>
2022-03-14 16:56:51 -0700

    q1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program that prints the alphabet in lowercase
+ * Description: prints the alphabet in lowercase\n
+ * Return 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * main 
+ * Description: 
+ * Return 0
+ */
+ 
+int main(void)
+	{
+	int i;
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
